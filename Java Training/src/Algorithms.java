45639ec75fd02e19cd09a5e523285d529f5235dc
Roberto Rodriguez <crazy_kalabaza@yahoo.com>
2016-03-27 22:59:25 -0600

    Initial commit of the Java training code.

---
+++ Java Training/src/Algorithms.java
@@ -0,0 +1,22 @@
+
+public class Algorithms {
+	static int IndexOf(int[] theArray, int targetValue) {
+		for (int index = 0; index < theArray.length; ++index)
+		{
+			if (theArray[index] == targetValue)
+				return index;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] theArray = {4, 2, 6, 1, 7, 3, 8, 5};
+		int value1 = 4;
+		int result = IndexOf(theArray, value1);
+		System.out.println("Looking for the value " + value1 + " in the array.");
+		if (result != -1)
+			System.out.println("Value found");
+		else
+			System.out.println("Value not found");
+	}
+}
